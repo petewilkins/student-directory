64826a7afbccd4f4c9bc7586c3ed0fc992b46eaa
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-03-02 16:08:35 +0000

    Loading data from the file

--- directory.rb
+++ directory.rb
@@ -11,6 +11,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -28,6 +29,8 @@ def process(selection)
             show_students
         when "3"
             save_students
+        when "4"
+            load_students
         when "9"
             exit
         else
@@ -37,6 +40,15 @@ end
 
 def months
     month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+end
+
+def load_students
+    file = File.open("students.csv", "r")
+    file.readlines.each do |line|
+        name, birth_year, hobby, cohort = line.chomp.split(',')
+        @students << {name: name, birth_year: birth_year, hobby: hobby, cohort: cohort.to_sym}
+    end
+    file.close
 end
 
 def input_students
